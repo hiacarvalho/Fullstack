f99c6c0db4a9130743f431d6a548181053f50c06
Hiac Carvalho <hiacarvalho@live.com>
2025-02-16 01:56:18 -0300

    new file:   learning/db.json 	modified:   learning/package-lock.json 	modified:   learning/package.json 	modified:   learning/src/App.js 	modified:   learning/src/index.js 	new file:   part2/phonebook/db.json 	modified:   part2/phonebook/package-lock.json 	modified:   part2/phonebook/package.json 	modified:   part2/phonebook/src/App.js

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,10 +1,26 @@
 import Note from './components/Note'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+axios.get('http://localhost:3001/notes').then((response) => {
+  const notes = response.data
+  console.log(notes)
+})
+
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect (efeito)');
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled (promessa resolvida)')
+      setNotes(response.data)
+    })
+  }, [])
+  
+  console.log('render (renderiza)', notes.length, 'notes (notas)')
 
   const addNote = (event) => {
     event.preventDefault()
